cabe026525f3063ff05c8fb5f4a3433a7cf607ba
HarshitRawat11 <59089335+HarshitRawat11@users.noreply.github.com>
2022-10-22 10:51:11 +0530

    Added code of Hello World in c#

---
+++ hello_world_c#.cs
@@ -0,0 +1,13 @@
+using System;
+namespace HelloWorldApp {
+	
+	class Hello {
+	
+		static void Main(string[] args) {
+		
+			Console.WriteLine("Hello World!");
+			
+			Console.ReadKey();
+		}
+	}
+}
